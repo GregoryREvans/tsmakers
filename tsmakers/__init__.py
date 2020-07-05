5344ddc1334147a600f651661a8523aa22f00e86
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-05 15:27:17 -0400

    Initializer.

--- tsmakers/__init__.py
+++ tsmakers/__init__.py
@@ -1,14 +1,31 @@
-from .BoundaryTimespanMaker import *
-from .CascadingTimespanMaker import *
-from .CompositeMusicSpecifier import *
-from .Cursor import *
-from .DependentTimespanMaker import *
-from .FloodedTimespanMaker import *
-from .HashCachingObject import *
-from .MusicSpecifier import *
-from .MusicSpecifierSequence import *
-from .PerformedTimespan import *
-from .SilentTimespan import *
-from .TaleaTimespanMaker import *
-from .TimespanMaker import *
-from .TimespanSpecifier import *
+from .BoundaryTimespanMaker import BoundaryTimespanMaker
+from .CascadingTimespanMaker import CascadingTimespanMaker
+from .Cursor import Cursor
+from .DependentTimespanMaker import DependentTimespanMaker
+from .FloodedTimespanMaker import FloodedTimespanMaker
+from .HashCachingObject import HashCachingObject
+from .PerformedTimespan import PerformedTimespan
+from .SilentTimespan import SilentTimespan
+from .TaleaTimespanMaker import TaleaTimespanMaker
+from .TimespanMaker import TimespanMaker
+from .TimespanSpecifier import TimespanSpecifier
+from .CompositeMusicSpecifier import CompositeMusicSpecifier
+from .MusicSpecifier import MusicSpecifier
+from .MusicSpecifierSequence import MusicSpecifierSequence
+
+__all__ = [
+    "BoundaryTimespanMaker",
+    "CascadingTimespanMaker",
+    "CompositeMusicSpecifier",
+    "Cursor",
+    "DependentTimespanMaker",
+    "FloodedTimespanMaker",
+    "HashCachingObject",
+    "MusicSpecifier",
+    "MusicSpecifierSequence",
+    "PerformedTimespan",
+    "SilentTimespan",
+    "TaleaTimespanMaker",
+    "TimespanMaker",
+    "TimespanSpecifier",
+]
