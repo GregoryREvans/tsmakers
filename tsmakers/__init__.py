53202c0e692c32419e437a01ba119889c283b02f
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-05 15:29:29 -0400

    Format.

--- tsmakers/__init__.py
+++ tsmakers/__init__.py
@@ -1,17 +1,17 @@
 from .BoundaryTimespanMaker import BoundaryTimespanMaker
 from .CascadingTimespanMaker import CascadingTimespanMaker
+from .CompositeMusicSpecifier import CompositeMusicSpecifier
 from .Cursor import Cursor
 from .DependentTimespanMaker import DependentTimespanMaker
 from .FloodedTimespanMaker import FloodedTimespanMaker
 from .HashCachingObject import HashCachingObject
+from .MusicSpecifier import MusicSpecifier
+from .MusicSpecifierSequence import MusicSpecifierSequence
 from .PerformedTimespan import PerformedTimespan
 from .SilentTimespan import SilentTimespan
 from .TaleaTimespanMaker import TaleaTimespanMaker
 from .TimespanMaker import TimespanMaker
 from .TimespanSpecifier import TimespanSpecifier
-from .CompositeMusicSpecifier import CompositeMusicSpecifier
-from .MusicSpecifier import MusicSpecifier
-from .MusicSpecifierSequence import MusicSpecifierSequence
 
 __all__ = [
     "BoundaryTimespanMaker",
