3ccc7b5392b0c47dffc7728e70009e484f8d49a0
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-12 19:56:44 -0400

    Setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+install_requires = ["abjad", "quicktions"]
+
 setup(
     name="tsmakers",
     version="0.1dev",
     packages=["tsmakers"],
+    install_requires=install_requires,
     license="Creative Commons Attribution-Noncommercial-Sharalike license",
     long_description=open("README.md").read(),
 )
